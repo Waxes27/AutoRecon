47f002e3e17c2a5471de7107c9d5296d12f7853a
waxes27 <nceba.dumasi@gmail.com>
2021-12-18 19:41:53 +0200

    naming scheme

--- scanssl.sh
+++ scanssl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 mkdir nmap
-mkdir directories
+mkdir directories/medium -p
+mkdir directories/small -p
+
 
 xterm -e nmap -sC -sV -oA nmap/initial $1 -vvv&
 xterm -e gobuster dir -u https://$1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -o directories/$1.txt &
 xterm -e gobuster dir -u https://$1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o directories/$1.txt &
+
+
 
 xterm -e python3 -m http.server 8000
