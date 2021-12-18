787454303dbf9bc4f4a80a7ec9dc3bd1d2075e11
waxes27 <nceba.dumasi@gmail.com>
2021-12-18 20:33:57 +0200

    refine

--- scan.sh
+++ scan.sh
@@ -6,9 +6,8 @@ mkdir directories/small -p
 
 
 xterm -e nmap -sC -sV -oA nmap/initial $1 -vvv&
-xterm -e gobuster dir -u http://$1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -o directories/$1.txt &
-xterm -e gobuster dir -u http://$1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o directories/$1.txt &
-
+xterm -e gobuster dir -u http://$1$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -o directories/small/$1-dirs.txt &
+xterm -e gobuster dir -u http://$1$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o directories/medium/$1-dirs.txt &
 
 
 xterm -e python3 -m http.server 8000
