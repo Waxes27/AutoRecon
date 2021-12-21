cb6e513d73dd391ab35bc4afbe9d5dc45dca64f1
waxes27 <nceba.dumasi@gmail.com>
2021-12-21 23:58:19 +0200

    unblock port 22

---
+++ spoofers/new_identity.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo ifconfig wlan0 down
+sudo macchanger -r wlan0
+sudo ifconfig wlan0 up
--- unblock_any.sh
+++ unblock_any.sh
@@ -1,2 +1,4 @@
 ssh -D 9090 -N -f root@waxes27.com
-/usr/bin/google-chrome --user-data-dir=/home/waxes27/proxy-profile --proxy-server=socks5://localhost:9090 www.whatismyip.com/
+/usr/bin/google-chrome --user-data-dir=/home/waxes27/proxy-profile --proxy-server=socks5://localhost:9090 www.whatismyip.com/ &
+
+/usr/bin/google-chrome --user-data-dir=/home/waxes27/proxy-profile --proxy-server=socks5://localhost:9090 www.spys.one/
